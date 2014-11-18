3b889a136676eee54772d90737e7dd68433bd7c5
Jonathan Rainer <jr776@york.ac.uk>
2014-11-18 10:15:44 +0000

    Created a very simple protocol with no radio control to get the Motes to simply blink a predefined LED with a defined duration.

--- open1_task1/src/open1_task1/SimpleSync.java
+++ open1_task1/src/open1_task1/SimpleSync.java
@@ -1,5 +1,41 @@
 package open1_task1;
 
+import com.ibm.saguaro.system.*;
+
 public class SimpleSync {
+	
+	// Timer to allow the LED to Blink
+	private static Timer  	tblink;
+	// Duration of blinking (i.e how long the LED should remain on for)
+	private static long		BLINK_DURATION = 500l;
+	
+	static
+	{
+		tblink = new Timer();
+		tblink.setCallback(new TimerEvent(null) 
+		{
+			@Override
+			public void invoke(byte param, long time) 
+			{
+				toggleLED((byte) 0x0, time);
+			}
+		});
+	}
+	
+	private static void toggleLED(byte param, long time)
+	{
+		if (LED.getState(param) == 1)
+		{
+            LED.setState(param, (byte)0);
+		}
+        else
+        {
+            LED.setState(param, (byte)1);
+            tblink.setAlarmBySpan(Time.toTickSpan(Time.MILLISECS, 
+            		BLINK_DURATION));
+        }
+	}
+	
+
 
 }
